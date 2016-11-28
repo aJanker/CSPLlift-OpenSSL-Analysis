d33f61bb083815573ee833127a64aeb4cbe04e7b
Andreas Janker <janker@fim.uni-passau.de>
2016-11-28 20:23:24 +0100

    intial commit

---
+++ openssl/minimalAES.c
@@ -0,0 +1,19 @@
+#include <openssl/evp.h>
+
+int main()
+{
+    EVP_CIPHER_CTX *ctx;
+    unsigned char key[32] = {0};
+    unsigned char iv[16] = {0};
+    unsigned char in[16] = {0};
+    unsigned char out[32]; /* at least one block longer than in[] */
+    int outlen1, outlen2;
+
+    EVP_CIPHER cipher = aes_256_cbc_spl();
+    EVP_EncryptInit(ctx, cipher, key, iv);
+    EVP_EncryptUpdate(ctx, out, &outlen1, in, sizeof(in));
+   EVP_EncryptFinal(ctx, out + outlen1, &outlen2);
+
+    printf("ciphertext length: %d\n", key);
+    return 0;
+}
