103ae17c06ad0c51cb479f5a0dbf4657758e0d82
Andreas Janker <janker@fim.uni-passau.de>
2016-11-29 13:23:35 +0100

    fixed path issues

--- openssl/evaluation_CodeCoverage_slurm.sh
+++ openssl/evaluation_CodeCoverage_slurm.sh
@@ -1,18 +1,34 @@
-#!/bin/sh
+#!/bin/bash
+#SBATCH -D /scratch/janker/Masterarbeit/CSPLlift-OpenSSL-Analysis/openssl
+#SBATCH --job-name=openssl
+#SBATCH -p chimaira
+#SBATCH -A spl
+#SBATCH --get-user-env
+#SBATCH -n 1
+#SBATCH -c 4
+#SBATCH --mem=15360
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=janker@fim.uni-passau.de
+#SBATCH --mem_bind=local
+#SBATCH --output=/scratch/janker/Masterarbeit/CSPLlift-OpenSSL-Analysis/openssl/codeLog
+#SBATCH --error=/scratch/janker/Masterarbeit/CSPLlift-OpenSSL-Analysis/openssl/codeErr
+#SBATCH --time=24:00:00
+#SBATCH --exclude=chimaira12,chimaira13,chimaira14,chimaira15,chimaira16,chimaira17
 
-PATH=$(pwd)
 
-flags="$PATH/../../TypeChef-Sampling-OpenSSL/openssl/minimalAES.c \
---bdd --dimacsFeaturePrefix \"\" --featureModelDimacs $PATH/../../TypeChef-Sampling-OpenSSL/openssl/OpenSSL.dimacs \
---platfromHeader $PATH/../../TypeChef-GNUCHeader/platform.h \
--I $PATH/../../TypeChef-GNUCHeader/usr_include/ \
--I $PATH/../../TypeChef-GNUCHeader/x86_64-linux-gnu/4.8/include \
--I $PATH/../../TypeChef-GNUCHeader/x86_64-linux-gnu/4.8/include-fixed \
--I $PATH/../../TypeChef-GNUCHeader/x86_64-linux-gnu \
---openFeat $PATH/../../TypeChef-GNUCHeader/openfeatures.txt \
--I $PATH/../../TypeChef-Sampling-OpenSSL/openssl/include \
+ABSPATH=$(pwd)
+
+flags="$ABSPATH/../../CSPLlift-OpenSSL-Analysis/openssl/minimalAES.c \
+--bdd --dimacsFeaturePrefix \"\" --featureModelDimacs $ABSPATH/../../CSPLlift-OpenSSL-Analysis/openssl/OpenSSL.dimacs \
+--platfromHeader $ABSPATH/../../TypeChef-GNUCHeader/platform.h \
+-I $ABSPATH/../../TypeChef-GNUCHeader/usr_include/ \
+-I $ABSPATH/../../TypeChef-GNUCHeader/x86_64-linux-gnu/4.8/include \
+-I $ABSPATH/../../TypeChef-GNUCHeader/x86_64-linux-gnu/4.8/include-fixed \
+-I $ABSPATH/../../TypeChef-GNUCHeader/x86_64-linux-gnu \
+--openFeat $ABSPATH/../../TypeChef-GNUCHeader/openfeatures.txt \
+-I $ABSPATH/../../CSPLlift-OpenSSL-Analysis/openssl/include \
 --spllift taint --spllift evalcoverage --spllift callgraph --spllift printvariants \
 --recordTiming --lexNoStdout \
---linkingInterface $PATH/../../TypeChef-Sampling-OpenSSL/openssl/CModuleInterface.interface"
+--linkingInterface $ABSPATH/../../CSPLlift-OpenSSL-Analysis/openssl/CModuleInterface.interface"
 
-sbatch -p chimaira  -A spl -n 1 -c 4 --mem=15360 --time=24:00:00  --mem_bind=local --output=$PATH/codeLog --error=$PATH/codeErrLog $PATH/../../CSPLlift/lift.sh $flags
+$ABSPATH/../../CSPLlift/lift.sh $flags
--- openssl/evaluation_CondCoverage.sh
+++ openssl/evaluation_CondCoverage.sh
@@ -1,18 +1,34 @@
-#!/bin/sh
+#!/bin/bash
+#SBATCH -D /scratch/janker/Masterarbeit/CSPLlift-OpenSSL-Analysis/openssl
+#SBATCH --job-name=openssl_cond
+#SBATCH -p chimaira
+#SBATCH -A spl
+#SBATCH --get-user-env
+#SBATCH -n 1
+#SBATCH -c 4
+#SBATCH --mem=15360
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=janker@fim.uni-passau.de
+#SBATCH --mem_bind=local
+#SBATCH --output=/scratch/janker/Masterarbeit/CSPLlift-OpenSSL-Analysis/openssl/condLog
+#SBATCH --error=/scratch/janker/Masterarbeit/CSPLlift-OpenSSL-Analysis/openssl/condErr
+#SBATCH --time=24:00:00
+#SBATCH --exclude=chimaira12,chimaira13,chimaira14,chimaira15,chimaira16,chimaira17
 
-PATH=$(pwd)
 
-flags="$PATH/../../TypeChef-Sampling-OpenSSL/openssl/minimalAES.c \
---bdd --dimacsFeaturePrefix \"\" --featureModelDimacs $PATH/../../TypeChef-Sampling-OpenSSL/openssl/OpenSSL.dimacs \
---platfromHeader $PATH/../../TypeChef-GNUCHeader/platform.h \
--I $PATH/../../TypeChef-GNUCHeader/usr_include/ \
--I $PATH/../../TypeChef-GNUCHeader/x86_64-linux-gnu/4.8/include \
--I $PATH/../../TypeChef-GNUCHeader/x86_64-linux-gnu/4.8/include-fixed \
--I $PATH/../../TypeChef-GNUCHeader/x86_64-linux-gnu \
---openFeat $PATH/../../TypeChef-GNUCHeader/openfeatures.txt \
--I $PATH/../../TypeChef-Sampling-OpenSSL/openssl/include \
+ABSPATH=$(pwd)
+
+flags="$ABSPATH/../../CSPLlift-OpenSSL-Analysis/openssl/minimalAES.c \
+--bdd --dimacsFeaturePrefix \"\" --featureModelDimacs $ABSPATH/../../CSPLlift-OpenSSL-Analysis/openssl/OpenSSL.dimacs \
+--platfromHeader $ABSPATH/../../TypeChef-GNUCHeader/platform.h \
+-I $ABSPATH/../../TypeChef-GNUCHeader/usr_include/ \
+-I $ABSPATH/../../TypeChef-GNUCHeader/x86_64-linux-gnu/4.8/include \
+-I $ABSPATH/../../TypeChef-GNUCHeader/x86_64-linux-gnu/4.8/include-fixed \
+-I $ABSPATH/../../TypeChef-GNUCHeader/x86_64-linux-gnu \
+--openFeat $ABSPATH/../../TypeChef-GNUCHeader/openfeatures.txt \
+-I $ABSPATH/../../CSPLlift-OpenSSL-Analysis/openssl/include \
 --spllift taint --spllift evalsingle --spllift callgraph --spllift printvariants \
 --recordTiming --lexNoStdout \
---linkingInterface $PATH/../../TypeChef-Sampling-OpenSSL/openssl/CModuleInterface.interface"
+--linkingInterface $ABSPATH/../../CSPLlift-OpenSSL-Analysis/openssl/CModuleInterface.interface"
 
-time $PATH/../../CSPLlift/lift.sh $flags
+$ABSPATH/../../CSPLlift/lift.sh $flags
