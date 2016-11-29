36fb181c0afe21e7d19489f348832f896350573c
Andreas Janker <an.janker@googlemail.com>
2016-11-29 13:26:28 +0100

    Removed slurm commands

--- openssl/evaluation_CodeCoverage.sh
+++ openssl/evaluation_CodeCoverage.sh
@@ -1,19 +1,4 @@
 #!/bin/bash
-#SBATCH -D /scratch/janker/Masterarbeit/CSPLlift-OpenSSL-Analysis/openssl
-#SBATCH --job-name=openssl
-#SBATCH -p chimaira
-#SBATCH -A spl
-#SBATCH --get-user-env
-#SBATCH -n 1
-#SBATCH -c 4
-#SBATCH --mem=15360
-#SBATCH --mail-type=FAIL
-#SBATCH --mail-user=janker@fim.uni-passau.de
-#SBATCH --mem_bind=local
-#SBATCH --output=/scratch/janker/Masterarbeit/CSPLlift-OpenSSL-Analysis/openssl/codeLog
-#SBATCH --error=/scratch/janker/Masterarbeit/CSPLlift-OpenSSL-Analysis/openssl/codeErr
-#SBATCH --time=24:00:00
-#SBATCH --exclude=chimaira12,chimaira13,chimaira14,chimaira15,chimaira16,chimaira17
 
 
 ABSPATH=$(pwd)
--- openssl/evaluation_CondCoverage.sh
+++ openssl/evaluation_CondCoverage.sh
@@ -1,19 +1,4 @@
 #!/bin/bash
-#SBATCH -D /scratch/janker/Masterarbeit/CSPLlift-OpenSSL-Analysis/openssl
-#SBATCH --job-name=openssl_cond
-#SBATCH -p chimaira
-#SBATCH -A spl
-#SBATCH --get-user-env
-#SBATCH -n 1
-#SBATCH -c 4
-#SBATCH --mem=15360
-#SBATCH --mail-type=FAIL
-#SBATCH --mail-user=janker@fim.uni-passau.de
-#SBATCH --mem_bind=local
-#SBATCH --output=/scratch/janker/Masterarbeit/CSPLlift-OpenSSL-Analysis/openssl/condLog
-#SBATCH --error=/scratch/janker/Masterarbeit/CSPLlift-OpenSSL-Analysis/openssl/condErr
-#SBATCH --time=24:00:00
-#SBATCH --exclude=chimaira12,chimaira13,chimaira14,chimaira15,chimaira16,chimaira17
 
 
 ABSPATH=$(pwd)
